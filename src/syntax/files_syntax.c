a69fff13b7853f22daf9dc1671488938220654dd
medel <moel-yag@student.1337.ma>
2025-05-08 14:37:21 +0100

    Update files_syntax.c

--- src/syntax/files_syntax.c
+++ src/syntax/files_syntax.c
@@ -1,5 +1,26 @@
 #include "../../includes/minishell.h"
 
+bool file_syntax(const char *input)
+{
+    int i = 0;
+
+    while (input[i])
+    {
+        if ((input[i] == '<' || input[i] == '>') && (input[i + 1] == '<' || input[i + 1] == '>'))
+        {
+            printf("Error: Invalid file redirection syntax\n");
+            return (false);
+        }
+        if ((input[i] == '<' || input[i] == '>') && (input[i + 1] == '\0' || input[i + 1] == '|' || input[i + 1] == '&'))
+        {
+            printf("Error: Missing file after redirection\n");
+            return (false);
+        }
+        i++;
+    }
+    return (true);
+}
+
 int files_syntax(t_lexer *lex)
 {
     char *msg = "minishell: syntax error near unexpected token `newline'";
